71326e159bafe95cd1b67b91b1daf879b37fa05e
MING <ming_fx@foxmail.com>
2018-12-24 22:26:42 +0800

    进度：comment的Controller未完成

--- src/main/java/com/nowcoder/wenda/controller/CommentController.java
+++ src/main/java/com/nowcoder/wenda/controller/CommentController.java
@@ -1,8 +1,16 @@
 package com.nowcoder.wenda.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CommentController {
+    @RequestMapping(path = {"/addComment"},method = {RequestMethod.POST})
+    public String addComment(@RequestParam("questionId") int questionId,
+                             @RequestParam("content") String content){
+        return null;
+    }
 
 }
